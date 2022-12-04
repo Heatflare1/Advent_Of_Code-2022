2716dd007799a0c729813cdfc463258e42ec2b92
Jisse Meruma <jisse@pop-os.localdomain>
2022-12-04 18:20:07 +0100

    removed some unused code...

--- day_04/part1.py
+++ day_04/part1.py
@@ -8,7 +8,7 @@ overlaps = 0
 rangemin = -1
 rangemax = -1
 for numbpairs in ids:
-	for i, numb in enumerate(numbpairs):
+	for numb in numbpairs:
 		if (rangemin >= int(numb[0]) and rangemax <= int(numb[1])):
 			overlaps += 1
 		elif (rangemin <= int(numb[0]) and rangemax >= int(numb[1])):
